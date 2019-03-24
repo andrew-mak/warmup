7dacd839777788afbe0ea8d52e6ea4785e89853f
Andrei <andrews.makarevich@gmail.com>
2019-03-24 22:03:22 +0300

    feat: implement convert function
    function converts C to F

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function warmup(temperature) {
-  // your implementation here
+  //your implementation here
+
+   var celsius=temperature;
+   var fahrenheit=celsius* 9 / 5 + 32;
+   //var message = cel+'\xB0C = ' + fahrenheit + ' \xB0F.';
+   //console.log(message);
+   return fahrenheit;
 };
